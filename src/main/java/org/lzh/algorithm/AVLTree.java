cea1583cc38b2df1ca18ebf0b6c4c70e86be3fc3
lizhaohui <lizhaohui836@126.com>
2017-01-09 10:15:25 +0800

    尝试merge

--- src/main/java/org/lzh/algorithm/AVLTree.java
+++ src/main/java/org/lzh/algorithm/AVLTree.java
@@ -18,17 +18,53 @@ public class AVLTree<T extends Comparable<T>> {
 
     public AVLTree(){this.root = null;}
     /**
-     * 插入节点
-     * @param tree
-     * @param node
+     * 插入节点，递归
+     * 1、根节点为空，创建根节点，高度+1
+     * 2、插入值大于root节点，插入至root右节点；小于则插入root左节点
+     * 3、循环递归2操作，直至根节点为空，则找到待插入的位置，插入节点并重新计算节点高度
+     * 4、插入完节点，检验左右子树是否非平衡；是，则进行调整
+     * @param root 树的根节点
+     * @param value 待插入节点的值
+     * @return 根节点
      */
-    private void insert(AVLTree<T> tree, AVLTreeNode<T> node){
-        if(tree.root == null){
-            tree.root = node;
-            return;
+    private AVLTreeNode<T> insert(AVLTreeNode<T> root, T value){
+        if(root == null){
+            root = new AVLTreeNode<T>(value, null, null);
         }
-        AVLTreeNode<T> tmp;
-        AVLTreeNode<T> x = tree.root;
+        else {
+            int tmp = value.compareTo(root.getValue());
+            //放入右子树
+            if(tmp > 0){
+                root.right = insert(root.right, value);
+                //插入后高度为2
+                if(height(root.right) - height(root.left) == 2){
+                    if(value.compareTo(root.right.getValue()) < 0){
+                        root = rlRotation(root); //属于右左的情况
+                    }
+                    else {
+                        root = rrRotation(root); //属于右右情况
+                    }
+                }
+            }
+            else if(tmp < 0){
+                root.left = insert(root.left, value);
+                //插入后高度为2
+                if(height(root.left) - height(root.right) == 2){
+                    if(value.compareTo(root.left.getValue()) < 0){
+                        root = llRotation(root); //属于右左的情况
+                    }
+                    else {
+                        root = lrRotation(root); //属于右右情况
+                    }
+                }
+            }
+            else {
+                System.out.println("不允许添加相同节点！");
+            }
+        }
+        //设置高度
+        root.height = max(height(root.left), height(root.right)) + 1;
+        return root;
     }
     /**
      * LeftLeft情况的旋转，返回旋转后的根节点
@@ -96,6 +132,10 @@ public class AVLTree<T extends Comparable<T>> {
         return 0;
     }
     class AVLTreeNode<T extends Comparable<T>>{
+        public T getValue() {
+            return value;
+        }
+
         T value;
         int height;
         AVLTreeNode<T> left;
