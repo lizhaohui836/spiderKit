43681d4fd8844285e1c8ab8e214950850ce854f1
lizhaohui <lizhaohui@kanzhun.com>
2017-02-24 15:28:48 +0800

    demo修改

--- src/main/java/org/lzh/lucene/Indexer.java
+++ src/main/java/org/lzh/lucene/Indexer.java
@@ -2,12 +2,10 @@ package org.lzh.lucene;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
-import org.apache.lucene.document.Document;
-import org.apache.lucene.document.Field;
-import org.apache.lucene.document.StringField;
-import org.apache.lucene.document.TextField;
+import org.apache.lucene.document.*;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.index.Term;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 import org.apache.lucene.util.Version;
@@ -20,16 +18,29 @@ import java.nio.charset.StandardCharsets;
  */
 public class Indexer {
     public static void main(String[] args) throws Exception {
-        if(args.length != 2){
-            throw new IllegalArgumentException("Usage: java " + Indexer.class.getName() + " <index dir> <data dir>");
+        String usage = "Usage: java " + Indexer.class.getName() + " <-index INDEX_PATH><-data DATA_PATH> [-update]";
+        String indexPath = "index";
+        String dataPath = null;
+        boolean create = true;
+        //参数检验与拼接
+        for(int i = 0; i < args.length; i++){
+            if("-index".equals(args[i])){
+                indexPath = args[i+1];
+            } else if("-data".equals(args[i])){
+                dataPath = args[i+1];
+            } else if("-update".equals(args[i])){
+                create = false;
+            }
         }
-        String indexDir = args[0];
-        String dataDIr = args[1];
+        //dataPath为空
+        if(dataPath == null){
+            throw new IllegalArgumentException(usage);
+        }
         long start = System.currentTimeMillis();
-        Indexer indexer = new Indexer(indexDir);
+        Indexer indexer = new Indexer(indexPath, create);
         int numIndex;
         try {
-            numIndex = indexer.index(dataDIr, new TextFilesFilter());
+            numIndex = indexer.index(dataPath, new TextFilesFilter());
         } finally {
             indexer.close();
         }
@@ -38,9 +49,16 @@ public class Indexer {
     }
 
     private IndexWriter writer;
-    public Indexer(String indexDir) throws IOException{
+    public Indexer(String indexDir, boolean create) throws IOException{
         Directory directory = FSDirectory.open(new File(indexDir));
         IndexWriterConfig config = new IndexWriterConfig(Version.LUCENE_48, new StandardAnalyzer(Version.LUCENE_48));
+        //是否为新建
+        if(create){
+            config.setOpenMode(IndexWriterConfig.OpenMode.CREATE);
+        }
+        else {
+            config.setOpenMode(IndexWriterConfig.OpenMode.CREATE_OR_APPEND);
+        }
         writer = new IndexWriter(directory, config);
     }
 
@@ -48,6 +66,13 @@ public class Indexer {
         writer.close();
     }
 
+    /**
+     * 针对目录递归进行创建索引
+     * @param dataDir 待创建索引的文件目录
+     * @param filter 文件系统
+     * @return 创建索引的数量
+     * @throws Exception
+     */
     public int index(String dataDir, FileFilter filter) throws Exception{
         File[] files = new File(dataDir).listFiles();
         for(File file : files){
@@ -68,11 +93,23 @@ public class Indexer {
         }
     }
 
+    /**
+     * 创建文档
+     * @param file
+     * @return document对象
+     * @throws Exception
+     */
     protected Document getDocument(File file) throws Exception{
         Document doc = new Document();
         String content = "";
         StringBuffer sb = new StringBuffer();
-        BufferedReader reader = new BufferedReader(new FileReader(file));
+        FileInputStream fis;
+        try {
+            fis = new FileInputStream(file);
+        } catch (FileNotFoundException e){
+            return null;
+        }
+        BufferedReader reader = new BufferedReader(new InputStreamReader(fis, StandardCharsets.UTF_8));
         while(content != null){
             content = reader.readLine();
             if(content == null){
@@ -81,17 +118,36 @@ public class Indexer {
             sb.append(content);
         }
         content = sb.toString();
-        doc.add(new StringField("contents",
+        System.out.println("file content: " + content);
+        doc.add(new TextField("contents",
                 content, Field.Store.YES));
+        doc.add(new LongField("modified", file.lastModified(), Field.Store.NO));
         doc.add(new TextField("text", reader));
         doc.add(new StringField("filename", file.getName(), Field.Store.YES));
         doc.add(new StringField("path", file.getPath(), Field.Store.YES));
         return doc;
     }
 
+    /**
+     * 对文件创建索引
+     * @param file
+     * @throws Exception
+     */
     private void indexFile(File file) throws Exception{
         System.out.println("Indexing " + file.getCanonicalPath());
         Document document = getDocument(file);
-        writer.addDocument(document);
+        if(document != null){
+            if(writer.getConfig().getOpenMode() == IndexWriterConfig.OpenMode.CREATE){
+                System.out.println("index adding " + file.getPath());
+                writer.addDocument(document);
+            }
+            else {
+                System.out.println("index updating " + file.getPath());
+                writer.updateDocument(new Term("path", file.getPath()), document);
+            }
+        }
+        else {
+            System.err.println("file get document error: " + file.getPath());
+        }
     }
 }
--- src/main/java/org/lzh/lucene/Searcher.java
+++ src/main/java/org/lzh/lucene/Searcher.java
@@ -8,10 +8,12 @@ import org.apache.lucene.index.Term;
 import org.apache.lucene.queryparser.classic.ParseException;
 import org.apache.lucene.queryparser.classic.QueryParser;
 import org.apache.lucene.search.*;
+import org.apache.lucene.search.Query;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 import org.apache.lucene.util.Version;
 
+import javax.management.*;
 import java.io.File;
 import java.io.IOException;
 
@@ -20,20 +22,24 @@ import java.io.IOException;
  */
 public class Searcher {
     public static void main(String[] args) throws IOException, ParseException {
-        if(args.length != 2){
-            throw new IllegalArgumentException("Usage: java" + Searcher.class.getName() + " <indexDir> <query>");
+        if(args.length != 3){
+            System.err.println("Your command is: " + args.toString());
+            throw new IllegalArgumentException("Usage: java" + Searcher.class.getName() + " <indexDir> <field> <query>");
         }
         String indexDir = args[0];
-        String query = args[1];
-        search(indexDir, query);
+        String field = args[1];
+        String query = args[2];
+        search(indexDir, field, query);
     }
 
-    public static void search(String indexDir, String q) throws IOException, ParseException {
+    public static void search(String indexDir, String field, String q) throws IOException, ParseException {
         DirectoryReader directoryReader = DirectoryReader.open(FSDirectory.open(new File(indexDir)));
         IndexSearcher is = new IndexSearcher(directoryReader);
-//        QueryParser queryParser = new QueryParser(Version.LUCENE_48, "contents", new StandardAnalyzer(Version.LUCENE_48));
+//        QueryParser queryParser = new QueryParser(Version.LUCENE_48, field, new StandardAnalyzer(Version.LUCENE_48));
 //        Query query = queryParser.parse(q);
-        Query query = new TermQuery(new Term("filename", q));
+        Query query = new TermQuery(new Term(field, q));
+//        Query query = new PrefixQuery(new Term(field, q));
+//        Query query = new WildcardQuery(new Term(field, q));
         long start = System.currentTimeMillis();
         TopDocs hits = is.search(query, 10);
         long end = System.currentTimeMillis();
